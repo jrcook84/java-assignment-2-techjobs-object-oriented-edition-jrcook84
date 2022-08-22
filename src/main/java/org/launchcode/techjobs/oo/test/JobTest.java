c927d327a87d7cae97682c6a9f804b883620e45a
peter <peter_cook@comcast.com>
2022-08-22 00:52:53 -0400

    started unit testing added 3 instances of Job class for testing

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -2,11 +2,37 @@ package org.launchcode.techjobs.oo.test;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.launchcode.techjobs.oo.*;
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+
+
+    Job jobOne = new Job();
+    Job jobTwo = new Job();
+    Job jobThree = new Job("Product tester", new Employer("ACME"),
+            new Location("Desert"), new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+
+    @Test
+public void testSettingJobId (){
+    assertTrue(jobOne.getId() != jobTwo.getId());
+
+}
+
+
+
+
+
+
 
 }
